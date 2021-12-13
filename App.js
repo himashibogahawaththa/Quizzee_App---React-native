6b70bd05551cf4eac940426864e523b07a8ed12a
Dhanushki Jinadari <87171356+dhanushkijinadari@users.noreply.github.com>
2021-12-13 21:55:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,39 +1,28 @@
-import * as React from 'react';
-import { Button, View , Image, StyleSheet} from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import AboutScreen from './screens/AboutScreen';
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from './screens/LoginScreen';
-import QuizScreen from './screens/QuizScreen';
-import LearnMoreScreen from './screens/LearnMoreScreen';
-import logo from '../quizzee-app/assets/logo.png';
+import React from 'react'
+import { View,Text,ImageBackground,Button} from 'react-native'
+import Style from './Style'
+const App =()=>{
+    return(
+        <View>
+        <ImageBackground source={require('./images/img-1.png')} style={Style.image}>
 
 
-const Drawer = createDrawerNavigator();
+          <Text style={Style.text}>Php Basics</Text>
+          <Text style={Style.text2}>Questions: 60                                 2.00 Min</Text>
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" screenOptions={{headerShadowVisible: false}}>
-        <Drawer.Screen name="Home" component={HomeScreen} options={{
-          headerStyle:{
-            backgroundColor: '#080A1E',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle:{
-            display: 'none'
-          }
-         }} />
-         <Drawer.Screen name="Quiz" component={QuizScreen} />
-         <Drawer.Screen name="Learn More" component={LearnMoreScreen} />
-        <Drawer.Screen name="About" component={AboutScreen} />
-        <Drawer.Screen name="Login" component={LoginScreen} options={{
-                drawerLabel: () => null,
-                title: null,
-                drawerIcon: () => null,
-            }}/>
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+          <Text style={Style.text1}>Q13. What is the purpose of constant() function?</Text>
+          <Text style={Style.text3}>function will return value</Text>
+          <Text style={Style.text4}>function will return value</Text>
+          <Text style={Style.text5}>function will return value</Text>
+          <Text style={Style.text6}>function will return value</Text>
+    <View style={Style.screenContainer}>
+      <Button title="Submit" />
+    </View>
+    
+        </ImageBackground>
+            
+        </View>
+    )
 }
+
+export default App;
